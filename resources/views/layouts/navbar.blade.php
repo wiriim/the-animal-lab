3c2703ee536efb06aba403aeef15f897b084a9d6
wiriim <51820876+wiriim@users.noreply.github.com>
2024-11-12 22:27:22 +0700

    Add content to home.blade.php
    Added fox background image, and explore button redirecting to animals
    page

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,22 +1,22 @@
-<nav class="navbar navbar-expand-lg bg-primary">
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">TheAnimalLab</a>
+      <a class="navbar-brand ms-3 text-light" href="{{ route('home') }}">TheAnimalLab</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+          <li class="nav-item me-3">
+            <a class="nav-link text-light {{Route::is('home') ? 'text-decoration-underline active' : ''}}" aria-current="page" href="{{ route('home') }}">Home</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('animals') }}">Animals</a>
+          <li class="nav-item me-3">
+            <a class="nav-link text-light {{Route::is('animals') ? 'text-decoration-underline active' : ''}}" href="{{ route('animals') }}">Animals</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Login</a>
+          <li class="nav-item me-3">
+            <a class="nav-link text-light" href="#">About Us</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Register</a>
+          <li class="nav-item me-3">
+            <a class="nav-link text-light" href="#">Login</a>
           </li>
         </ul>
       </div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Hello World!</h1>
+    
+    <div class="home-bg">
+        <div class="content-mask">
+            <div class="content display-1">The Animal Lab</div>
+            <a href="{{ route('animals') }}" class="content btn btn-outline-light mt-1">Explore</a>
+        </div>
+        
+    </div>
+
 @endsection
 
